fb2d3b146645a8bfb964dfef3183f7979424e0af
zhangpeng <realmax.goole@gmail.com>
2024-11-01 15:58:47 +0800

    feat: 初始化 init

---
+++ src/content/components/app.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="head">this is the content</div>
+</template>
+
+<script setup>
+const a = ref('');
+</script>
+
+<style scoped>
+.head{
+	color: red;
+	position: fixed;
+	z-index: 100001;
+	right: 10px;
+	bottom: 10px;
+}
+</style>
---
+++ src/popup/components/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <el-button type="danger" @click="test">测试按钮</el-button>
+  </div>
+</template>
+<script setup>
+const test = () => {
+  chrome.tabs.getSelected(null, function(tab){
+  chrome.tabs.sendRequest(tab.id, { popAction: "Test"});
+  });
+}
+</script>
+<style scoped>
+.wrapper{
+  width: 100px;
+  height: 100px;
+  background-color: rgb(248, 214, 242);
+}
+</style>
