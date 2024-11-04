175ed205a0ea0130bcdf2afff359c7c149db6266
zhangpeng <realmax.goole@gmail.com>
2024-11-04 08:48:11 +0800

    feat: 增加ts 校验配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,11 +28,8 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
+      0
+      
     ],
     'no-unused-vars': [
       'error',
--- src/popup/components/app.vue
+++ src/popup/components/app.vue
@@ -3,18 +3,17 @@
     <el-button type="danger" @click="test">测试按钮</el-button>
   </div>
 </template>
-<script setup>
-const ass = ref()
-const test = () => {
-  chrome.tabs.getSelected(null, function(tab){
-  chrome.tabs.sendRequest(tab.id, { popAction: "Test"});
-  });
-}
+<script setup lang="ts">
+  const test = () => {
+    chrome.tabs.getSelected(0, function (tab) {
+      chrome.tabs.sendRequest(tab.id || 0, { popAction: 'Test' });
+    });
+  };
 </script>
 <style scoped>
-.wrapper{
-  width: 100px;
-  height: 100px;
-  background-color: rgb(248, 214, 242);
-}
+  .wrapper {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(248, 214, 242);
+  }
 </style>
