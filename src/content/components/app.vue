c170675ca62e6c6041533f414399470e73c9da12
zhangpeng <realmax.goole@gmail.com>
2024-11-07 10:23:09 +0800

    fix: 测试 husky 校验

--- src/content/components/app.vue
+++ src/content/components/app.vue
@@ -1,17 +1,15 @@
 <template>
-	<div class="head">this is the content</div>
+  <div class="head">this is the content</div>
 </template>
 
-<script setup>
-const a = ref('');
-</script>
+<script setup></script>
 
 <style scoped>
-.head{
-	color: red;
-	position: fixed;
-	z-index: 100001;
-	right: 10px;
-	bottom: 10px;
-}
+  .head {
+    color: red;
+    position: fixed;
+    z-index: 100001;
+    right: 10px;
+    bottom: 10px;
+  }
 </style>
